1067ab5e65e3adbd2308e37087eb259b3e358757
strongwillow <clear.tsai@gmail.com>
2020-12-26 18:35:10 +0100

    Add support for altitude

--- Example/LocationSimulator/ViewController.swift
+++ Example/LocationSimulator/ViewController.swift
@@ -12,7 +12,7 @@ import LocationSimulator
 
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
 
-    let locationSimulator = LSLocationManager(builtInLocationFileName: "SanFrancisco.gpx")
+    let locationSimulator = LSLocationManager(builtInLocationFileName: "EindhovenBiking.gpx")
     @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         locationSimulator.delegate = self
         locationSimulator.startUpdatingLocation()
         mapView.delegate = self
+    }
+    
+    func printLocation(loc: CLLocation) {
+        print(loc.coordinate.latitude, loc.coordinate.longitude, loc.altitude)
     }
     
     var lastCoord: CLLocationCoordinate2D?
@@ -37,8 +41,10 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         
         let cam = MKMapCamera(lookingAtCenter: lastCoord!, fromDistance: 1500, pitch: 0, heading: 0)
         mapView.setCamera(cam, animated: true)
-        print(locations)
         
+        for loc in locations {
+            printLocation(loc: loc)
+        }
     }
     
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
--- LocationSimulator/Classes/GPXFile.swift
+++ LocationSimulator/Classes/GPXFile.swift
@@ -4,6 +4,7 @@ import CoreLocation
 public final class GPXFile: NSObject {
 
     private let REGEXP_GPX_COORD = try! NSRegularExpression(pattern: "lat=\"(-?\\d+\\.\\d+)\" lon=\"(-?\\d+\\.\\d+)\"", options: NSRegularExpression.Options.anchorsMatchLines)
+    private let REGEXP_GPX_ALT = try! NSRegularExpression(pattern: "<ele>(-?\\d+\\.?\\d*)</ele>", options: NSRegularExpression.Options.anchorsMatchLines)
 
     private var locations = [CLLocation]()
     private var url: URL
@@ -39,6 +40,9 @@ public final class GPXFile: NSObject {
         
         let strstr = str as String
         
+        var coordinates = [CLLocationCoordinate2D]()
+        var altitudes = [Double]()
+        
         REGEXP_GPX_COORD.enumerateMatches(in: strstr, options: NSRegularExpression.MatchingOptions.reportCompletion, range: range, using: { (res, flag, stop) -> Void in
             
             if let r = res {
@@ -47,8 +51,23 @@ public final class GPXFile: NSObject {
                 let lng = (str.substring(with: r.range(at: 2)) as NSString).doubleValue
                 
                 let coord = CLLocationCoordinate2D(latitude: lat, longitude: lng)
-                locations.append(CLLocation(coordinate: coord, altitude: 0, horizontalAccuracy: 0, verticalAccuracy: 0, timestamp: Date(timeIntervalSince1970: 0)))
+                coordinates.append(coord)
             }
         })
+        
+        REGEXP_GPX_ALT.enumerateMatches(in: strstr, options: NSRegularExpression.MatchingOptions.reportCompletion, range: range, using: { (res, flag, stop) -> Void in
+            
+            if let r = res {
+  
+                let alt = (str.substring(with: r.range(at: 1)) as NSString).doubleValue
+                altitudes.append(alt)
+            }
+        })
+        
+        assert(coordinates.count == altitudes.count)
+        
+        for i in 0...coordinates.count - 1 {
+            locations.append(CLLocation(coordinate: coordinates[i], altitude: altitudes[i], horizontalAccuracy: 0, verticalAccuracy: 0, timestamp: Date(timeIntervalSince1970: 0)))
+        }        
     }
 }
